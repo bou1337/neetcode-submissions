06b57f53b7fb949c359a8f2af3f52b63d665213d
Ismail AIT BOUGNSA <aitbougnsa.ismail@gmail.com>
2026-08-17 19:48:00 +0100

    Add: two-integer-sum - submission-21

---
+++ Data Structures & Algorithms/two-integer-sum/submission-21.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        dic = {}
+        result = []
+
+        for i  in range(len(nums)):
+            
+            if target - nums[i] not in dic : 
+                dic[nums[i]]= i
+            else:
+                result.extend([dic[target - nums[i]],i])
+                break 
+        return result 
+            
