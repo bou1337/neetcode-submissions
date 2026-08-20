a67bc7d39675fa59fc5ff4db240774a00a1def99
Ismail AIT BOUGNSA <aitbougnsa.ismail@gmail.com>
2026-08-20 17:28:53 +0100

    Add: is-subsequence - submission-8

---
+++ Data Structures & Algorithms/is-subsequence/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        j = 0 
+        c = 0
+        for  i in range(len(s)):
+
+            while j < len(t):
+                if s[i]==t[j]:
+                    c +=1
+                    j +=1
+                    break 
+                j +=1 
+            if j == len(t):
+                break 
+                
+
+        return c == len(s)
+    
+
+
+            
