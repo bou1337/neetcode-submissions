45068d5c262ec95be16cbe5c922ca84261eccb0b
Ismail AIT BOUGNSA <aitbougnsa.ismail@gmail.com>
2026-08-25 15:21:03 +0100

    Add: is-palindrome - submission-16

---
+++ Data Structures & Algorithms/is-palindrome/submission-16.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        l = len(s) -1 
+        r = 0 
+
+        while r < l :
+            if (s[r].isalpha() or s[r].isdigit())==False :
+                r +=1 
+
+                continue 
+            if (s[l].isalpha() or s[l].isdigit())== False :
+                l -=1
+                continue
+                 
+            if  s[r].upper()!=s[l].upper():
+                return False 
+            r +=1
+            l -=1
+        return True 
+    
